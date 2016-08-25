9a08ca9cdb8d97dc45dead84489ec0d274de76c1
hishidag <hishidag@gmail.com>
2016-08-25 11:34:18 +0900

    オリジナルサイト

--- Original/src/main/resources/schema.sql
+++ Original/src/main/resources/schema.sql
@@ -3,17 +3,17 @@ DROP TABLE IF EXISTS memo CASCADE;
 DROP TABLE IF EXISTS user CASCADE;
 
 CREATE TABLE user(
-	userID		int				PRIMARY KEY AUTO_INCREMENT,
+	userID		int			PRIMARY KEY AUTO_INCREMENT,
 	name		varchar(31)		NOT NULL,
 	password	varchar(31)		NOT NULL,
-	email		varchar(255)	NOT NULL UNIQUE,
+	email		varchar(255)		NOT NULL UNIQUE,
 	newDate		timestamp		default CURRENT_TIMESTAMP
 );
 
 CREATE TABLE word(
-	wordID		int				PRIMARY KEY AUTO_INCREMENT,
-	userID		int				NOT NULL,
-	word		varchar(255)	NOT NULL,
+	wordID		int			PRIMARY KEY AUTO_INCREMENT,
+	userID		int			NOT NULL,
+	word		varchar(255)		NOT NULL,
 	translation	text			NOT NULL,
 	language	varchar(15)		NOT NULL,
 	newDate		timestamp		default CURRENT_TIMESTAMP, 
@@ -21,8 +21,8 @@ CREATE TABLE word(
 );
 
 CREATE TABLE memo(
-	memoID		int				PRIMARY KEY AUTO_INCREMENT,
-	userID		int				NOT NULL,
+	memoID		int			PRIMARY KEY AUTO_INCREMENT,
+	userID		int			NOT NULL,
 	memo		text			NOT NULL,
 	language	varchar(15)		NOT NULL,
 	newDate 	timestamp		default CURRENT_TIMESTAMP,
@@ -30,5 +30,5 @@ CREATE TABLE memo(
 );
 
 INSERT INTO user(name,password,email)				VALUES('a','a','a'); 
-INSERT INTO word(userID,word,translation,language)	VALUES('1','a','a','English'); 
+INSERT INTO word(userID,word,translation,language)		VALUES('1','a','a','English'); 
 INSERT INTO memo(userID,memo,language)				VALUES('1','test','English'); 
